d23cdc7adb870548385334ad2a4be15df19680bc
Sam Niyazi <sam2003@niyazi.com>
2021-02-17 17:53:31 -0600

    Completed M09-06 and M09-07 (Create store)

---
+++ src/redux/actions/reducers/courseReducer.js
@@ -0,0 +1,19 @@
+
+
+export default function courseReducer( state = [], action ) {
+
+    switch ( action.type ) {
+
+
+        case "CREATE_COURSE":
+
+            //return [...state, { ...action.course} ];
+            return [ ...state, action.course ];
+
+
+        default:
+
+            return state;
+    }
+
+} 
