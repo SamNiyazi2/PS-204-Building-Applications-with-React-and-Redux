0be43853c788826cbcf0301db619ad030e89d425
Sam Niyazi <sam2003@niyazi.com>
2021-02-17 11:07:58 -0600

    Completed M05-06.

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
+import Header from './common/Header';
 
 function App() {
 
     return (
 
         <div className="container-fluid">
+            <Header></Header>
             <Route exact path="/" component={HomePage} />
             <Route path="/about" component={AboutPage} />
         </div>
