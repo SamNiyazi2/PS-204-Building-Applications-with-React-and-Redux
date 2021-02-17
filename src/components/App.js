eb77fb2ec36caada6b123bb0f93c1f18d8e04956
Sam Niyazi <sam2003@niyazi.com>
2021-02-17 11:14:49 -0600

    Completed M05-07.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 
 
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import Header from './common/Header';
+import PageNotFound from './PageNotFound';
 
 function App() {
 
@@ -12,8 +13,11 @@ function App() {
 
         <div className="container-fluid">
             <Header></Header>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/about" component={AboutPage} />
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route component={PageNotFound} />
+            </Switch>
         </div>
     );
 
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+
+const PageNotFound = () => <h1>Oops! &nbsp; Page not found.</h1>
+
+export default PageNotFound;
