714f5db61c42cb0859dd102f768150eb1231ad26
Sam Niyazi <sam2003@niyazi.com>
2021-02-18 10:39:58 -0600

    Completed M10-07.

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,9 +1,34 @@
 
 import * as actionTypes from './actionTypes';
+import * as courseApi from '../../api/courseApi';
+
 
 export function createCourse( course ) {
     return {
         type: actionTypes.CREATE_COURSE,
         course
+    }
+}
+
+
+export function loadCoursesSuccess( courses ) {
+
+    return { type: actionTypes.LOAD_COURSES_SUCCESS, courses };
+
+}
+
+
+export function loadCourses() {
+
+    return function ( dispatch ) {
+
+        return courseApi.getCourses().then( courses => {
+
+            dispatch( loadCoursesSuccess( courses ) );
+
+        } ).catch( response => {
+
+            throw response;
+        } );
     }
 }
