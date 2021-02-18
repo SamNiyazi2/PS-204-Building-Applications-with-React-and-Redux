92fca18f388e56d191ff4f5352c0c28e41790734
Sam Niyazi <sam2003@niyazi.com>
2021-02-17 18:57:32 -0600

    Completed M09-08.

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 
 import App from './components/App';
 
+import configureStore from './redux/actions/confiureStore';
+import { Provider as ReduxProvider } from 'react-redux';
+
+const store = configureStore();
+
 
 render(
 
+  <ReduxProvider store={store}>
     <Router>
-        <App />
+      <App />
     </Router>
+  </ReduxProvider>,
 
-    , document.getElementById( 'app' ) );
+  document.getElementById('app')
+
+);
