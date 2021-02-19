e8431a9ed30e2a8ff7ed32898203b7e0726cbf05
Sam Niyazi <sam2003@niyazi.com>
2021-02-19 05:35:41 -0600

    Completed M13-03.

--- ssn__06__Dev_start_tests.cmd
+++ ssn__06__Dev_start_tests.cmd
@@ -1,5 +1,10 @@
+
+rem 02/19/2021 05:25 am - SSN
+
 cd %~dp0
 
-call C:\sams\ps\KillProcess\KillProcess_Using_MainWindowTitle.cmd "*%ssnalert% - Jest Testing - 20200820-1738*"
+set scriptKey_20210219_0525=%~1-20210219-0525
+
+call C:\sams\ps\KillProcess\KillProcess_Using_MainWindowTitle.cmd "*scriptKey_20210219_0525*"
 
 start %1 npm test
