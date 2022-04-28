d777ff72f877fedb47b00bcc108333d3355c5225
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-04-28 05:38:09 -0500

    Pusing to GitHub.

--- ssn__07__Prod_build_client.cmd
+++ ssn__07__Prod_build_client.cmd
@@ -1,13 +1,25 @@
 
+rem 02/20/2021 06:51 am - SSN
+
 cd %~dp0
 
-call C:\sams\ps\KillProcess\KillProcess_Using_MainWindowTitle.cmd "*%ssnalert% - webpack.config.prod-20200821-1646*"
-call C:\sams\ps\KillProcess\KillProcess_Using_MainWindowTitle.cmd "*%ssnalert% - http-server-20200821-1928*"
+set scriptKey_20210220_0652=%~1-20210220-0652
+set port=3178
+
+call C:\sams\ps\KillProcess\KillProcess_Using_MainWindowTitle.cmd "*%scriptKey_20210220_0652%*"
+
+call C:\sams\ps\KillProcess\KillProcess_Using_PortNo.cmd %port%
 
 rem %1 build:with / build:without INCLUDE_BUNDLE_ANALYZER
 
-start npm  run build:%1
-echo Wait for signal...
-waitfor productionVer20200822 /T 60
-echo Starting webpage...
-start chrome http://p3130.nonbs.net:3130
+echo %1
+echo %2 
+ 
+
+rem start npm  run build:%1
+start npm  run build
+
+rem echo Wait for signal...
+rem waitfor productionVer20200822 /T 60
+rem echo Starting webpage...
+start chrome http://p3178.nonbs.org:%port%
